0c5e85ff7b7c04bb830e76ddcc6bffb1eb8bbf66
Anoop K R <talktoanu.kr@gmail.com>
2025-11-09 12:28:57 +0000

    created date_dimension model

---
+++ models/date_dimension.sql
@@ -0,0 +1,21 @@
+with cte as
+(
+select
+
+to_timestamp(started_at) as started_at,-- convert to time stamp
+date(to_timestamp(started_at)) as date_started_at,
+hour(to_timestamp(started_at)) as hour_started_at,
+case when
+dayname(to_timestamp(started_at)) in ('Sat','Sun') then 'Weekend'
+else 'Business day' end  as day_type,
+CASE when month(to_timestamp(started_at)) in (12,1,2) then 'Winter'
+     when month(to_timestamp(started_at)) in (3,4,5) then 'Spring'
+     when month(to_timestamp(started_at)) in (6,7,8) then 'Summer'
+     else 'Autumn' end as station_of_year
+
+
+from {{ source('demo', 'bike_data') }}
+
+)
+
+select * from cte
